2fe2b7c3a6d53431ca415a6c48f2431e371ffbba
yanxun <yanxun@moviewisdom.cn>
2017-06-29 17:18:14 +0800

    yx

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,27 @@
 var express = require('express');
 var router = express.Router();
+var path = require('path')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   //res.render('login', { title: 'login' });
-    console.log('get/')
-    res.sendFile("./public/login.html")
+    var options = {
+        root: path.resolve(__dirname, '../public/'),
+        dotfiles: 'deny',
+        headers: {
+            'x-timestamp': Date.now(),
+            'x-sent': true
+        }
+    };
+    res.sendFile("login.html", options, function (err) {
+        if (err) {
+            console.log(err);
+            res.status(err.status).end();
+        }
+        else {
+            console.log('Sent:', "login.html");
+        }
+    });
 });
 
 module.exports = router;
